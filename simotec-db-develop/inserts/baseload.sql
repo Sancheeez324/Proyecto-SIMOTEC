78c68772af6a696a3ea77c6d33cea159db2174dc
Sancheeez324 <pablo.sanchez.m2002@gmail.com>
2025-02-26 17:59:04 -0300

    Delete simotec-db-develop directory
    Signed-off-by: Sancheeez324 <pablo.sanchez.m2002@gmail.com>

--- simotec-db-develop/inserts/baseload.sql
+++
@@ -1,9 +0,0 @@
--- Insertar CADMINS y USUARIOS
-INSERT INTO cadmins
-(name, email, created_at)
-VALUES('cadmin1', 'panchober27@gmail.com', current_timestamp());
-
-INSERT INTO users
-(username, email, created_at)
-VALUES('usuario1', 'panchober27@gmail.com', current_timestamp());
-
